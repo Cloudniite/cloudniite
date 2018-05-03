0231fdb2ffa4f27e7a752f191b6d012e9db5bd09
haris799 <muhammad.sheikh93@yahoo.com>
2018-05-03 17:01:16 -0400

    Initial Files

--- C4/lambdaController.js
+++ C4/lambdaController.js
@@ -1,58 +1,89 @@
 const resources = require('./aws-stack-resources');
 const AWS = require('aws-sdk');
+const functionList = require('./listFunction');
 
 var lambda;
-
 const lambdaController = {};
 const tagGroups = {};
 
 function pullParams(funcName) {
     this.FunctionName = funcName,
-    this.InvocationType = 'RequestResponse',
-    this.LogType = 'None',
-    this.Payload = '{"source" : "C4-serverless"}'
+        this.InvocationType = 'RequestResponse',
+        this.LogType = 'None',
+        this.Payload = '{"source" : "C4-serverless"}'
 };
 
-lambdaController.configure = (IdentityPoolId, apiVersion = '2015-03-31') => {
+lambdaController.configure = (region, IdentityPoolId, apiVersion = '2015-03-31') => {
+    AWS.config.update({ region: region });
     AWS.config.credentials = new AWS.CognitoIdentityCredentials({ IdentityPoolId: IdentityPoolId });
-    lambda = new AWS.Lambda({ apiVersion: apiVersion });
+    lambda = new AWS.Lambda({ region: region, apiVersion: apiVersion });
 }
 
-lambdaController.createTagGroup = (tagGroup, ...rest) => {
-    const functionsArr = [];
-    resources.StackResourceSummaries.forEach(resource => {
-        if (rest.includes(resource.LogicalResourceId)) {
-            functionsArr.push(resource.PhysicalResourceId);
-        }
+lambdaController.getAwsFunctions = function (...rest) {
+    const awsFunctionNames = [];
+    functionList.Functions.forEach(func => {
+        if (rest.includes(func.FunctionName.split('-')[1])) awsFunctionNames.push(func.FunctionName);
     })
+    return awsFunctionNames;
+}
 
-    tagGroups[tagGroup] = functionsArr;
+lambdaController.warmupFunctions = function (timer = null, ...rest) {
+
+    var functions = this.getAwsFunctions(...rest);
+    const createfunc = () => {
+        var newFunctions = functions.map((func) => {
+            return new Promise((resolve) => {
+                lambda.invoke(new pullParams(func), (error, data) => {
+                    if (error) {
+                        throw error;
+                    } else {
+                        console.log(data);
+                        resolve();
+                    }
+                });
+            })
+        });
+        return newFunctions;
+    }
+
+    var promiseCall = () => {
+        Promise.all(createfunc())
+            .then(() => console.log(`Warmup of function/s ${rest} complete`))
+            .catch((error) => { console.error(`FAILED: Warmup of function/s ${rest} failed, ${error}`) });
+    }
+    promiseCall();
+    if (timer !== null) setInterval(() => { promiseCall(); }, (timer * 60000));
+}
+
+lambdaController.createTagGroup = function (tagGroup, ...rest) {
+    tagGroups[tagGroup] = this.getAwsFunctions(...rest);
 };
 
-lambdaController.warmupTagGroup = (tagGroup) => {
+lambdaController.warmupTagGroup = (timer = null, tagGroup) => {
     const functions = tagGroups[tagGroup];
+    const createFunc = () => {
+        var newFunctions = functions.map((func) => {
+            return new Promise((resolve) => {
+                lambda.invoke(new pullParams(func), (error, data) => {
+                    if (error) {
+                        throw error;
+                    } else {
+                        resolve();
+                    }
+                });
+            })
+        });
+        return newFunctions;
+    }
 
-    var newFunctions = functions.map((func) => {
-        console.log('Running this function: ', func);
+    const promiseCall = () => {
+        Promise.all(createFunc())
+            .then(() => console.log(`Warmup of category ${tagGroup} complete`))
+            .catch((error) => { console.error(`FAILED: Warmup of category ${tagGroup} failed: ${error}`) });
+    }
 
-        return new Promise((resolve) => {
-
-            lambda.invoke(new pullParams(func), (error, data) => {
-                if (error) {
-                    console.log("ERROR: ", error);
-                    resolve();
-                } else {
-                    console.log(`Invoked ${func}:`, data.Payload);
-                    resolve();
-                }
-            });
-        })
-    });
-
-    Promise.all(newFunctions).then(() => console.log('done'));
+    promiseCall();
+    if (timer !== null) setInterval(() => { promiseCall() }, (timer * 60000));
 }
 
-lambdaController.pullParamsBuild =
-
-
-    module.exports = lambdaController;
+module.exports = lambdaController;
--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const path = require('path');
 const body = require('body-parser');
-const lambda = require('./getLambda.js');
 const lambdaController = require('./C4/lambdaController.js');
 
 const app = express();
 
-lambdaController.configure('us-east-1:77063b48-4177-4e13-a3d7-50657c0c503e');
+lambdaController.configure('us-east-1','us-east-1:77063b48-4177-4e13-a3d7-50657c0c503e');
 
 app.get('/index.js', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.js'));
@@ -20,16 +19,10 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
-lambdaController.createTagGroup("#HelloWorld", "TestFunction1", "TestFunction2");
-lambdaController.createTagGroup("#HelloWorld1", "TestFunction3");
-lambdaController.warmupTagGroup("#HelloWorld");
-
-lambdaController.uzerId = "";
-
-app.get('/warmUp', lambda.warmUp);
-app.get('/getLambda1', lambda.getLambda1);
-app.get('/getLambda2', lambda.getLambda2);
-app.get('/getLambda3', lambda.getLambda3);
+// lambdaController.createTagGroup("#HelloWorld", "TestFunction1", "TestFunction2");
+// lambdaController.createTagGroup("#HelloWorld1", "TestFunction3");
+lambdaController.warmupFunctions(0.1,"TestFunction1", "TestFunction2");
+// lambdaController.warmupTagGroup(0.1, "#HelloWorld");
 
 app.listen(3000, () => {
     console.log("Listening on PORT");

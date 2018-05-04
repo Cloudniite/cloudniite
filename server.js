8ff79fbe9e65c1fb903e9e52137a7b9f1bd1d55e
StephenGrable1 <stephengrable@gmail.com>
2018-05-04 16:20:01 -0400

    add cloudwatch logs

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const lambdaController = require('./C4/index.js');
 const functionList = require('./awsLambda/listFunction.json');
 const app = express();
 
-lambdaController.configure('us-east-1','us-east-1:77063b48-4177-4e13-a3d7-50657c0c503e');
+lambdaController.configure('us-east-1','us-east-1:c7bc97de-e707-41b9-aa4a-b12ee24e5817');
 lambdaController.setFunctionList(functionList);
 
 app.get('/index.js', (req, res) => {
@@ -22,8 +22,8 @@ app.get('/', (req, res) => {
 
 lambdaController.createTagGroup("#HelloWorld", "TestFunction1", "TestFunction2");
 lambdaController.createTagGroup("#HelloWorld1", "TestFunction3");
-lambdaController.warmupFunctions(0.1,"TestFunction1");
-lambdaController.warmupTagGroup(0.1, "#HelloWorld1");
+lambdaController.warmupFunctions(3,"TestFunction2");
+lambdaController.warmupTagGroup(3, "#HelloWorld1");
 
 app.listen(3000, () => {
     console.log("Listening on PORT");

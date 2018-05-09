9eed2b4e2fcc5cd284432f4df8e9139e03e2cb63
StephenGrable1 <stephengrable@gmail.com>
2018-05-09 15:34:28 -0400

    add comments and update express example

--- examples/expressExample/appController/appControler.js
+++
@@ -1,7 +0,0 @@
-var appController = {};
-
-appController.goToLogin = function(req, res, next) {
-    //do the warmup call here
-}
-
-module.exports = appController;
--- examples/expressExample/server.js
+++ examples/expressExample/server.js
@@ -9,9 +9,15 @@ lambdaController.configure('us-east-1','us-east-1:77063b48-4177-4e13-a3d7-50657c
 lambdaController.setFunctionList(functionList, "dev");
 lambdaController.createTagGroup("#HelloWorld", "TestFunction4", "TestFunction5");
 
+//This is a custom route for specifically for development 
+//Go to this route to view all your tag groups and AWS Lambda function information
 app.get('/getHtmlViz', lambdaController.getHtmlViz);
 
 
+//Landing page routes
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
 
 app.get('/index.js', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.js'));
@@ -21,28 +27,25 @@ app.get('/index.css', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.css'));
 });
 
+
+//Signup and Login Page Routes
 app.get('/signup', (req, res) => {
+    //Here you can warm up functions that are likely to be invoked when a user goes to this page
     // lambdaController.warmupTagGroup(null, "#HelloWorld");
-    res.sendFile(path.join(__dirname, 'signupPage.html'));
+    res.sendFile(path.join(__dirname, './loginSignupPages/signupPage.html'));
 });
 
 app.get('/login', (req, res) => {
     // lambdaController.warmupTagGroup(null, "#HelloWorld");
-    res.sendFile(path.join(__dirname, 'loginPage.html'));
+    res.sendFile(path.join(__dirname, './loginSignupPages/loginPage.html'));
 });
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-
+//Media fetching routes
 app.get('/appMedia/guitar-stock.jpg', (req, res) => {
     res.sendFile(path.join(__dirname, './appMedia//guitar-stock.jpg'));
 });
 
 
-// lambdaController.createTagGroup("#HelloWorld1", "TestFunction6");
-lambdaController.warmupFunctions(null,"TestFunction6");
 
 app.listen(3000, () => {
     console.log("Listening on PORT");
